2c38fae0e3bf8ddfaec91c003ca098f144ab95b6
jkancc111 <jkancc3@gmail.com>
2024-09-21 18:12:35 +0700

    Add files via upload

---
+++ r2UxREm9o9EU5Zv.lua
@@ -0,0 +1,21 @@
+if game.PlaceId == 18481040458 then -- Factory RNG
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/main/FactoryRNG.lua",true))()
+elseif game.PlaceId == 17534163435 then -- Dungeon RNG
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/main/DungeonRNG.lua",true))()
+elseif game.PlaceId == 16466634211 then -- Lick Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/main/LickSimulator.lua",true))()
+elseif game.PlaceId == 16178787698 then -- Block Eaters
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/main/BlockEaters.lua",true))()
+elseif game.PlaceId == 3101667897 then -- Legend Of Speed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/main/LegendOfSpeed.lua",true))()
+elseif game.PlaceId == 18130504174 then -- Fat Race
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/main/FatRace.lua",true))()
+elseif game.PlaceId == 4480809144 then -- Granny Multiplayer
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/GrannyMultiplayer.txt",true))()
+elseif game.PlaceId == 16993432698 then -- Impossible Glass Bridge
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/ImpossibleGlassBridge.txt",true))()
+elseif game.PlaceId == 16981421605 then -- Reborn As Swordman
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/RebornAsSwordman.lua",true))()
+elseif game.PlaceId == 16280073867 then -- Build A Bridge Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/MakeABridgeSim.txt",true))()
+end
