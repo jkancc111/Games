8393a88c3780fa70aab5bd02b03e17fab8b986da
jkancc111 <jkancc3@gmail.com>
2024-10-06 01:29:45 +0700

    Update r2UxREm9o9EU5Zv.lua

--- r2UxREm9o9EU5Zv.lua
+++ r2UxREm9o9EU5Zv.lua
@@ -21,5 +21,17 @@ elseif game.PlaceId == 16280073867 then -- Build A Bridge Simulator
 elseif game.PlaceId == 17415466926 then -- Dragon Merge Tycoon
     loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/DragonMergeTycoon.txt",true))()
 elseif game.PlaceId == 13926416231 then -- Duck Army
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/DuckArmy.txt",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/DuckArmy.txt",true))()  -------------------------------------------- NEW ------------------------------------
+elseif game.PlaceId == 18768679013 then -- Every Click +1 Speed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/EveryClick%2B1Speed.txt",true))()
+elseif game.PlaceId == 18920893671 then -- Mining Tycoon
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/MiningTycoon.txt",true))()
+elseif game.PlaceId == 18778623849 then -- Meme Race
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/MemeRace.txt",true))()
+elseif game.PlaceId == 2248408710 then -- Destruction Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/DestructionSimulator.txt",true))()
+elseif game.PlaceId == 9150032572 then -- Clicking Gods
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/ClickingGods.txt",true))()
+elseif game.PlaceId == 18134655434 then -- Strongest Smacker Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/StrongestSmackSim.txt",true))()
 end
