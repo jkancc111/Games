12228c535842c60fe26f52715fe37c843a41d866
jkancc111 <jkancc3@gmail.com>
2024-11-14 18:22:47 +0700

    Update r2UxREm9o9EU5Zv.lua

--- r2UxREm9o9EU5Zv.lua
+++ r2UxREm9o9EU5Zv.lua
@@ -54,9 +54,6 @@ local function createGameButton(placeName, placeId, scriptUrl)
 end
 
 -- Create buttons for each game
-
-createGameButton("[🏴‍☠️] Fisch", 16732694052, "https://raw.githubusercontent.com/jkancc111/Games/main/http_proxies.txt")
-
 createGameButton("[UPD] Pixel Prisons ⛏️", 134877494536867, "https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/PixelPrison.txt")
 createGameButton("[RELEASE] Anime Shadow", 89438510123061, "https://raw.githubusercontent.com/jkancc111/Games/refs/heads/main/AnimeShadow.txt")
 createGameButton("Factory RNG", 18481040458, "https://raw.githubusercontent.com/jkancc111/Games/main/FactoryRNG.lua")
